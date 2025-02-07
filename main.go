6422b72ae2122c2338b0ad293dc499d84783e00e
DenzeL2004 <work20022004@mail.ru>
2025-02-07 20:02:30 +0300

    Change init and declaraion to :=

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var intVar int = 42
+	intVar := 42
 	fmt.Printf("Value int variable: %d. Type: %T\n", intVar, intVar)
 
-	var floatVar float64 = 3.14
+	floatVar := 3.14
 	fmt.Printf("Value float variable: %g. Type: %T\n", floatVar, floatVar)
 
-	var boolVar bool = true
+	boolVar := true
 	fmt.Printf("Value bool variable: %t. Type: %T\n", boolVar, boolVar)
 
-	var strVar string = "forty two"
+	strVar := "forty two"
 	fmt.Printf("Value string variable: %s. Type: %T\n", strVar, strVar)
 }
